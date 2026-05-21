1a87dedbc957431eb8412fff533f4773823d78f0
nickhertzog <nick.hertzog@gmail.com>
2026-05-21 16:37:11 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import BasketballGame from "./App.jsx";
+import App from "./App.jsx";
+import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BasketballGame />
+    <App />
   </React.StrictMode>
 );
